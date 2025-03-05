8157476356069e5ce875e8f52d2002b7318bda71
dinnerjin <sio015@naver.com>
2025-03-05 15:51:13 +0900

    vite & chakra-ui setting

---
+++ dapp/src/App.tsx
@@ -0,0 +1,11 @@
+import { Button } from "@chakra-ui/react";
+
+function App() {
+  return (
+    <Button colorPalette={"orange"} loading={true}>
+      Hello, Chakra UI!
+    </Button>
+  );
+}
+
+export default App;
